83845e621374fe29fab905d176c8ab9a2aa0e2c7
Viet Nguyen <viethoang.nguyen@mail.utoronto.ca>
2024-10-21 20:02:41 -0400

    update download_data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,27 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from FiveThrityEight
+# Author: Viet Nguyen
+# Date: 21 October 2024
+# Contact: viethoang.nguyen@utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: NA
+# Any other information needed? NA
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+raw_data <- read_csv("/Users/nguyenviet/Documents/term-paper-2/data/01-raw_data/president_polls.csv")
 
-
+clean_data <- raw_data |> select(pollster, display_name, 
+                                 pollster_rating_id, numeric_grade, pollscore,
+                                 transparency_score, state, start_date, end_date, 
+                                 sample_size, election_date, 
+                                 party, answer, candidate_name, pct) %>%
+  filter(pollster=="Siena/NYT") %>% filter(!is.na(state))
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(clean_data, "/Users/nguyenviet/Documents/term-paper-2/data/01-raw_data/clean_data.csv")
