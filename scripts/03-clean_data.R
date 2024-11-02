3bc6a67ad23b3766e63de8799a7a5157e4a942fb
Doran Wang <doran.wang@mail.utoronto.ca>
2024-11-02 02:42:35 -0400

    Upload by Doran Wang
    -Change the 'Rproj' name
    -Update the '02-download_data.R' and '03-clean_data.R'
    -Upload the NYT_data.csv in 'data' file
    -Upload the example.R in 'models' file

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,24 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the president polls data to the Siena/NYT pollster only data [...UPDATE THIS...]
+# Author: 
+# Date:
+# Contact: 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Access to the president_polls.csv first
 
 #### Workspace setup ####
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/01-raw_data/president_polls.csv")
 
-cleaned_data <-
+NYT_data <-
   raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
+  select(pollster,methodology,population,population_full,sample_size,numeric_grade,pollscore) |>
+  filter(pollster == "Siena/NYT") |>
   tidyr::drop_na()
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(NYT_data, "data/02-analysis_data/NYT_data.csv")
+
