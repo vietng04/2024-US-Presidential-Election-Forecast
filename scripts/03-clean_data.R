0944a85d1d1328942dc72555b03a66df8bdae404
Doran Wang <doran.wang@mail.utoronto.ca>
2024-11-03 23:14:45 -0500

    Update by Doran
    -Delete stater files
    -Update paper.qmd and paper.pdf
    -Upload new clean_data

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Cleans the president polls data to the Siena/NYT pollster only data [...UPDATE THIS...]
+# Purpose: Cleans the president polls data
 # Author: 
 # Date:
 # Contact: 
@@ -8,10 +8,18 @@
 
 #### Workspace setup ####
 library(tidyverse)
-
-#### Clean data ####
+library(janitor)
+library(tidyr)
 raw_data <- read_csv("data/01-raw_data/president_polls.csv")
 
+#### Clean data ####
+cleaned_data <-
+  raw_data |>
+  janitor::clean_names() |>
+  select(answer, pct, pollster, methodology, population, population_full, sample_size, numeric_grade, pollscore, party,transparency_score) |>
+  tidyr::drop_na()
+  
+#### Clean data with "NYT" pollster only ####
 NYT_data <-
   raw_data |>
   janitor::clean_names() |>
@@ -19,6 +27,12 @@ NYT_data <-
   filter(pollster == "Siena/NYT") |>
   tidyr::drop_na()
 
+#### Model data ####
+model_data <-
+  clean_data |>
+  select(answer,pct,sample_size,numeric_grade,transparency_score,pollscore)
+
 #### Save data ####
 write_csv(NYT_data, "data/02-analysis_data/NYT_data.csv")
-
+write_csv(cleaned_data, "data/02-analysis_data/cleaned_data.csv")
+write_csv(model_data, "data/02-analysis_data/model_data.csv")
